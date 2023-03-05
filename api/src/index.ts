8bfeb651362fe2bcb9e420f95190d1bde7d93c9d
Satish <satish.appanasamy@gmail.com>
2023-03-05 21:18:47 +1100

    Init Commit

---
+++ api/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (request, response) => {
+  response.send('Hello, GraphQL!');
+});
+
+app.listen(port, () => {
+  console.log(`Running a server at http://localhost:${port}`);
+});
